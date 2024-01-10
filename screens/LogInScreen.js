47cced02417619c662e006cc71cec17f853983eb
Eren AVAR <eren.avar@hotmail.com>
2024-01-10 06:13:20 +0300

    Added Login Operations

--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -1,15 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import AuthContent from "../components/AuthContent";
+import Loading from "../components/Loading";
+import { login } from "../util/Auth";
 
 export default function LogInScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    await login(email, password);
+    setIsAuthenticating(false);
+  }
+  if (isAuthenticating) {
+    return <Loading message={"Please Wait..."} />;
+  }
+
   return (
     <View>
-      <AuthContent isLogin />
+      <AuthContent isLogin onAuthenticate={loginHandler} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({});
-
-
--- util/Auth.js
+++ util/Auth.js
@@ -2,11 +2,10 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyBdFYdkNQEYvyaQkAqkLxzc8cwEfhKzNQw";
 
-export async function createUser(email, password) {
+async function authenticate(mode, email, password) {
   const response = await axios
     .post(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" +
-        API_KEY,
+      `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`,
       {
         email: email,
         password: password,
@@ -14,4 +13,12 @@ export async function createUser(email, password) {
       }
     )
     .catch((error) => console.log(error.response.request._response));
+}
+
+export async function createUser(email, password) {
+  return authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }
