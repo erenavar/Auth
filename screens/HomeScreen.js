6865961e38e681ac2423f1b7eea43a6ce05b0c89
Eren AVAR <eren.avar@hotmail.com>
2024-01-22 15:42:37 +0300

    Changed HomeScreen Css

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,10 +3,22 @@ import React from "react";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreenn</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome</Text>
+      <Text>You Have Successfully Logged In</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+});
