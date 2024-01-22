2543457ec0f79a51ef1edff060df5a775acce0af
Eren AVAR <eren.avar@hotmail.com>
2024-01-22 15:32:35 +0300

    Added AsyncStorage

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContext = createContext({
   token: "",
@@ -10,11 +11,23 @@ export const AuthContext = createContext({
 function AuthContextProvider({ children }) {
   const [authToken, setAuthToken] = useState(null);
 
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem("token");
+      if (storedToken) {
+        setAuthToken(storedToken);
+      }
+    }
+    fetchToken();
+  }, []);
+
   function authenticate(token) {
     setAuthToken(token);
+    AsyncStorage.setItem("token", token);
   }
   function logOut() {
     setAuthToken(null);
+    AsyncStorage.removeItem("token");
   }
   const value = {
     token: authToken,
@@ -24,8 +37,5 @@ function AuthContextProvider({ children }) {
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
-
-
-
 
 export default AuthContextProvider;
